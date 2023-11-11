543f1bbdf5b75e93df82cc4ee708c671cd29df5f
Duong Le <duole93@live.com>
2023-11-11 13:42:08 -0600

    add event to buttons, game logic, code refactor
    - startBtn - initialize the game
    - continue -  continue game after result each round
    - 5 rounds game logic. return final result of whoever win 3 rounds first or who has the most win at the end
    
    - minor adjust in style

--- js/rps-ui.js
+++ js/rps-ui.js
@@ -1,75 +1,124 @@
-const rule = {'rock':'scissor', 'scissor':'paper', 'paper':'rock'};
-const choices = ['rock','paper','scissor'];
-let rounds=0;
+const rule = { rock: "scissor", scissor: "paper", paper: "rock" };
+const choices = ["rock", "paper", "scissor"];
+const ROUND_MAX = 5;
+let round = 1;
+let compWin = 0;
+let playerWin = 0;
+let draw = 0;
 
-const btnPlayerChoices = document.querySelectorAll('.player-choices-container button') 
-const btnFiveRound = document.querySelector('.btn-five-round');
-const btnContinue = document.querySelector('.btn-continue');
-const btnQuit = document.querySelector('.btn-quit');
+//Button
+const btnPlayerChoices = document.querySelectorAll(
+	".player-choices-container button"
+);
+const btnFiveRound = document.querySelector(".btn-five-round");
+const btnContinue = document.querySelector(".btn-continue");
+const btnQuit = document.querySelector(".btn-quit");
+const btnStart = document.querySelector(".btn-start");
 
-const playerChoiceContainer = document.querySelector('.player-choices-container');
-const startModesContainer = document.querySelector('.start-modes-container');
-const continueContainer = document.querySelector('.continue-container');
-const resultContainer = document.querySelector('.result-container');
-const winnerDiv = document.querySelector('.result-winner');
-const resultDiv = document.querySelector('.result-msg');
-const btnStart = document.querySelector('.btn-start');
-
-
+//containers
+const playerChoiceContainer = document.querySelector(
+	".player-choices-container"
+);
+const resultContainer = document.querySelector(".result-container");
+const finalDiv = document.querySelector(".result-title");
+const winnerDiv = document.querySelector(".result-winner");
+const msgDiv = document.querySelector(".result-msg");
 
 //start btn
-btnStart.addEventListener('click', ()=>{
-    btnStart.style.display='none';
-    setTimeout(()=>{startModesContainer.style.display='flex'},200);
-})
+btnStart.addEventListener("click", () => {
+    //Initialize Game
+	round = 1;
+	compWin = 0;
+	playerWin = 0;
+	draw = 0;
 
-//5 round mode
-btnFiveRound.addEventListener('click',()=>{
-    startModesContainer.style.display='none';
-    playerChoiceContainer.style.display='flex';
-    resultContainer.style.display='block';
-    resultDiv.textContent = 'pick your poision';
-    rounds=5;
+    //Display Player Choices and message
+	btnStart.classList.add("display-none");
+	btnQuit.classList.add("display-none");
+	resultContainer.classList.remove("display-none");
+	playerChoiceContainer.classList.remove("display-none");
+
+    finalDiv.textContent = "";
+	winnerDiv.textContent = `Round ${round}`;
+	msgDiv.textContent = "pick your poision";
+
+
+
+
 });
 
 //continue
-btnContinue.addEventListener('click', ()=>{
-    continueContainer.style.display='none';
-    playerChoiceContainer.style.display='flex';
-    winnerDiv.style.display='none';
-    resultDiv.textContent = 'pick your poision';    
-})
+btnContinue.addEventListener("click", () => {
+    //if its not End game 
+	if (round <= ROUND_MAX) {
+        //show choices 
+		btnContinue.classList.toggle("display-none");
+		btnQuit.classList.toggle("display-none");
+		playerChoiceContainer.classList.toggle("display-none");
+		winnerDiv.textContent = `Round ${round}`;
+		msgDiv.textContent = "pick your poision";
+	} 
+    //END game
+    else if (playerWin === 3 || compWin == 3 || round > ROUND_MAX) {
+        //Toggle BUTTON
+		btnStart.classList.toggle("display-none");
+		btnContinue.classList.toggle("display-none");
+		btnQuit.classList.toggle("display-none");
+
+        //Display Winner
+		finalDiv.textContent = "FINAL RESULT";
+		msgDiv.textContent = `Player ${playerWin} : Computer ${compWin}`;
+		if (playerWin > compWin) {
+			winnerDiv.textContent = `Player Win!`;
+		} else if (compWin > playerWin) {
+			winnerDiv.textContent = `Computer Win!`;
+		} else {
+			winnerDiv.textContent = `Draw!`;
+		}
+	}
+});
 
 //quit
-btnQuit.addEventListener('click', ()=>{
-    playerChoiceContainer.style.display='none';
-    continueContainer.style.display='none';
-    startModesContainer.style.display='none';
-    resultContainer.style.display='none';
-    btnStart.style.display='block';
-})
-btnPlayerChoices.forEach((choice)=>{
-    choice.addEventListener('click',(event)=>{
-        playerChoiceContainer.style.display='none';
-        const player = event.target.className;
-        const comp = choices[Math.floor(Math.random()*3)];
-        const winner = getWinner(player,comp);
-        winnerDiv.textContent = winner.winner;
-        resultDiv.textContent = winner.reason;
-        winnerDiv.style.display='flex';
-        resultDiv.style.display='flex';
-        continueContainer.style.display='flex';
-    })
-})
+btnQuit.addEventListener("click", () => {
+	//
+});
 
-//0 = draw, 1 = player1, -1 = computer
-function getWinner(player,comp){
-    if(player=== comp)
-        return {winner:'It\'s Draw!', reason:`Both picked ${player}`};
-    if(rule[player]===comp){
-        return {winner:'Player Win!', reason:`${player} beats ${comp}`};
-    }
-    else    
-        return {winner:'Computer Win!', reason:`${comp} beats ${player}`};
+//player choices
+btnPlayerChoices.forEach((choice) => {
+	choice.addEventListener("click", (event) => {
+
+        //not end game
+		if (round <= ROUND_MAX) {
+            //compare choices
+			const player = event.target.className;
+			const comp = choices[Math.floor(Math.random() * 3)];
+			const winner = getWinner(player, comp);
+
+            //display game status
+			finalDiv.textContent = `Player ${playerWin} Draw ${draw} Computer ${compWin}`;
+			winnerDiv.textContent = winner.winner;
+			msgDiv.textContent = winner.reason;
+
+			playerChoiceContainer.classList.toggle("display-none");
+			btnContinue.classList.toggle("display-none");
+			btnQuit.classList.toggle("display-none");
+			round++;
+		}
+	});
+});
+
+//return result messages
+function getWinner(player, comp) {
+	if (player === comp) {
+		draw++;
+		return { winner: "It's Draw!", reason: `Both picked ${player}` };
+	}
+
+	if (rule[player] === comp) {
+		playerWin++;
+		return { winner: "Player Win!", reason: `${player} beats ${comp}` };
+	} else {
+		compWin++;
+		return { winner: "Computer Win!", reason: `${comp} beats ${player}` };
+	}
 }
-
